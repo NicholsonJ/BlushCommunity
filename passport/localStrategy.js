0511a0cb25977a997807a1e604a4738d499b2a0f
J Nicholson <jenicholson.nc@gmail.com>
2018-09-01 14:44:31 +0200

    opening the project

---
+++ passport/localStrategy.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User          = require('../models/User');
+const bcrypt        = require('bcrypt');
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+  }, 
+  (username, password, next) => {
+    User.findOne({ username }, (err, foundUser) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      if (!foundUser) {
+        next(null, false, { message: 'Incorrect username' });
+        return;
+      }
+
+      if (!bcrypt.compareSync(password, foundUser.password)) {
+        next(null, false, { message: 'Incorrect password' });
+        return;
+      }
+
+      next(null, foundUser);
+    });
+  }
+));
