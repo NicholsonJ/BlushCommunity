ea66869a8dbd96c00dd63071504c44fde785677e
Bhavana <bhavanavenugopal2@gmail.com>
2018-09-03 19:05:16 +0200

    add file upload on feed page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 const User = require('../models/User');
 const Selfie = require('../models/Selfie');
+const uploadCloud = require('../config/cloudinary.js');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -17,17 +19,21 @@ router.get('/feed', ensureLoggedIn('/auth/login'), (req, res, next) => {
   });
 });
 
-router.get('/Selfie', ensureLoggedIn('/auth/login'), (req, res, next) => {
+router.get('/selfie', ensureLoggedIn('/auth/login'), (req, res, next) => {
   res.render('selfie');
 });
 
-router.post('/Selfie', ensureLoggedIn('/auth/login'), (req, res, next) => {
+
+router.post('/selfie',uploadCloud.single("selfPic"), ensureLoggedIn('/auth/login'), (req, res, next) => {
+  console.log("req.file", req.file);
+  
   const selfieInfo = {
     title: req.body.title,
-    image: req.body.selfPic,
+    image: req.file.url,
     comment: req.body.comment,
     products: req.body.products,
     _user: req.user._id
+    
   };
   console.log(selfieInfo);
   Selfie.create(selfieInfo).then(selfieFromDb => {
