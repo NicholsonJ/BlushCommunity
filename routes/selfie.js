836fe614496cd204088212a7197e0bf396fa7b3e
J Nicholson <jenicholson.nc@gmail.com>
2018-09-04 15:25:50 +0200

    fix upload error with selfie form

--- routes/selfie.js
+++ routes/selfie.js
@@ -20,7 +20,7 @@ router.post(
       title: req.body.title,
       image: req.file.url,
       comment: req.body.comment,
-      _products: req.body.products,
+      // _products: req.body.products,
       _user: req.user._id
     };
     console.log(selfieInfo);
