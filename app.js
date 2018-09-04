01ab1339dd5943ddefe51b2a1aabe2cacf962d5f
Bhavana <bhavanavenugopal2@gmail.com>
2018-09-04 10:03:51 +0200

    add selfie.hbs bootstrap

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ hbs.registerHelper('ifUndefined', (value, options) => {
 });
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Vibe_me';
 
 // Enable authentication using session + passport
 app.use(
