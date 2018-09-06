caf6512ee3e063c3e7d5387981abef87d651777a
Bhavana <bhavanavenugopal2@gmail.com>
2018-09-06 20:50:24 +0200

    fix database

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 
-mongoose
-  .connect(
-    'mongodb://localhost/makeup',
-    { useNewUrlParser: true }
-  )
+// mongoose
+//   .connect(
+  //     'mongodb://localhost/makeup',
+  //     { useNewUrlParser: true }
+  //   )
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
